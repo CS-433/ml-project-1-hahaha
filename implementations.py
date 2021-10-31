7768fe5c4842dd8e4508f6f17782f81a300f070d
edwin628 <1143934303@qq.com>
2021-10-31 22:19:47 +0100

    first commit

---
+++ implementations.py
@@ -0,0 +1,7 @@
+'''File which contains all function implementations from table 1
+of step 2 as well as auxiliar functions used in the project'''
+
+#Necessary imports:
+import numpy as np
+import itertools
+from proj1_helpers import predict_labels
---
+++ run.py
@@ -0,0 +1,7 @@
+# Script which "produces exactly the same .csv predictions which you used in
+# your best submission to the competition on Kaggle".
+
+import numpy as np
+from implementations import *
+from helpers import *
+from proj1_helpers import *
